b39cbbed44cdc25b69ae9bfe8586789d22c48257
Sigit Kuncoro <sigit.kunc@gmail.com>
2018-10-16 22:42:52 +0700

    feat(Components): Add Layout.css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.css";
 
 const Layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, BackDrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
