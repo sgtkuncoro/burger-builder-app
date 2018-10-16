dd23c2ce6cc94f5d2471692f164c76ada1015da2
Sigit Kuncoro <sigit.kunc@gmail.com>
2018-10-17 00:52:56 +0700

    feat(Burger): Calculating the Ingredient Sum Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,26 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
+  // create array of key
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(key => {
+      // create new array from amount key
+      // props.ingredients[key] is a Number
+      return [...Array(props.ingredients[key])].map((_, i) => {
+        return <BurgerIngredient key={key + i} type={key} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type={`bread-top`} />
-      <BurgerIngredient type={`cheese`} />
-      <BurgerIngredient type={`meat`} />
+      {transformedIngredients}
       <BurgerIngredient type={`bread-bottom`} />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controll</div>
       </Aux>
     );
