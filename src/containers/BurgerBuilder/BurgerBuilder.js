9cbc358a08ed2a91d3d683a8db14954b5abd9fe4
Sigit Kuncoro <sigit.kunc@gmail.com>
2018-10-20 00:07:22 +0700

    feat(Components): Adding the Build Control Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -14,7 +15,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controll</div>
+        <BuildControls />
       </Aux>
     );
   }
