b7fc8bfcad1602c1cd188fb3385330d71f368e0b
sgtkuncoro <sigit.kunc@gmail.com>
2018-11-14 16:13:12 +0700

    feat(Order): Add order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Aux from "../../../hoc/Auxiliary";
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+    return (
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>: {props.ingredients[igKey]}
+      </li>
+    );
+  });
+
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to Checkout?</p>
+    </Aux>
+  );
+};
+
+OrderSummary.propTypes = {};
+
+export default OrderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import { Object } from "core-js";
 
 const INGREDIENT_PRICES = {
@@ -73,6 +75,9 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
+        <Modal>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
